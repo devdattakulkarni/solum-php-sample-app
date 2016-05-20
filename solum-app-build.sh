76204c8365038cf3e9782af65feabd5a9cd0090b
Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
2016-05-20 11:47:36 -0500

    Changed hello.php to index.php

--- solum-app-build.sh
+++ solum-app-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 if [ -d "/var/www/html" ]; then
-    cp hello.php /var/www/html/.
-fi
-
-if [ -d "/var/www/public" ]; then
-    cp hello.php /var/www/public/.
+    cp index.php /var/www/html/.
 fi
 
--- start.sh
+++ start.sh
@@ -2,25 +2,25 @@
 
 cd /var/www/html
 
-if [ ! -e .htaccess ]; then
-    # NOTE: The "Indexes" option is disabled in the php:apache base image
-    cat > .htaccess <<EOF
-    # BEGIN WordPress
-    <IfModule mod_rewrite.c>
-    RewriteEngine On
-    RewriteBase /
-    RewriteRule ^hello\.php$ - [L]
-    RewriteCond %{REQUEST_FILENAME} !-f
-    RewriteCond %{REQUEST_FILENAME} !-d
-    RewriteRule . /hello.php [L]
-    </IfModule>
-    # END WordPress
-EOF
+#if [ ! -e .htaccess ]; then
+#    # NOTE: The "Indexes" option is disabled in the php:apache base image
+#    cat > .htaccess <<EOF
+#    # BEGIN WordPress
+#    <IfModule mod_rewrite.c>
+#    RewriteEngine On
+#    RewriteBase /
+#    RewriteRule ^hello\.php$ - [L]
+#    RewriteCond %{REQUEST_FILENAME} !-f
+#    RewriteCond %{REQUEST_FILENAME} !-d
+#    RewriteRule . /hello.php [L]
+#    </IfModule>
+#    # END WordPress
+#EOF
 
-chown www-data:www-data .htaccess
+#chown www-data:www-data .htaccess
 
-fi
+#fi
 
-chown www-data:www-data hello.php
+chown www-data:www-data index.php
 
 exec apache2-foreground
